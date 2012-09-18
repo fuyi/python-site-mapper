74dce392f1dabf89b2d17adc0c3f4caca69cff44
fuyi <yi@whaam.com>
2012-09-18 14:54:56 +0200

    add multi-thread implementation to fetch web page

--- mapper.py
+++ mapper.py
@@ -1,7 +1,33 @@
 from urllib2 import * # http client
 from BeautifulSoup import BeautifulSoup # html parser
 from urlparse import urlparse # url parser
+import threading
+import Queue
 import time
+
+class GraphThread(threading.Thread):
+    def __init__(self, p, graph, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.graph = graph
+        self.p = p
+    def run(self):
+        while True:
+            # print self.getName() + " is running"
+            link = self.queue.get() # get link from queue
+            page = self.graph.findPage(link) if self.graph.findPage(link) else self.graph.findDeadPage(link)
+            if page:
+                pass
+            else:
+                page = Page(link)
+                if not page.dead_page:
+                    self.graph.addPage(page)
+                else:
+                    self.graph.addDeadPage(page)
+            if not page.dead_page:
+                self.graph.addNext(self.p,page)
+            self.graph.buildGraph(page)
+            self.queue.task_done()
 
 class Graph():
     """class for page graph"""
@@ -14,44 +40,54 @@ class Graph():
             # step 1, mark page as visited
             p.visited = True
             # step 2, add current page to graph if not in graph yet
-            if not self.__findPage(p.url):
-                self.__addPage(p)
+            if not self.findPage(p.url):
+                self.addPage(p)
             # step 3, generate pages for each internal link and add to page next, add page to graph if it is a new page
+            # method 1: multithread implementation
+            queue = Queue.Queue()
             for link in p.in_links:
-                page = self.__findPage(link) if self.__findPage(link) else self.__findDeadPage(link)
-                if page: # page already exist on graph, do nothing
-                    pass
-                else: 
-                    page = Page(link) # page not exist, create new page
-                    if not page.dead_page:
-                        self.__addPage(page) # add new page to graph if not dead page
-                    else: # page is dead page, add to deadpage list
-                        self.__addDeadPage(page)
-                if not page.dead_page:
-                    self.__addNext(p,page) # add page to current pages next list if not deadpage
-                self.buildGraph(page) # recursively build graph 
+                queue.put(link)
+            for i in range(10):
+                t = GraphThread(p,self,queue)
+                t.setDaemon(True)
+                t.start()
+            queue.join()
+            # method 2: synchronized implementation
+            # for link in p.in_links:
+            #     page = self.findPage(link) if self.findPage(link) else self.findDeadPage(link)
+            #     if page: # page already exist on graph, do nothing
+            #         pass
+            #     else: 
+            #         page = Page(link) # page not exist, create new page
+            #         if not page.dead_page:
+            #             self.addPage(page) # add new page to graph if not dead page
+            #         else: # page is dead page, add to deadpage list
+            #             self.addDeadPage(page)
+            #     if not page.dead_page:
+            #         self.addNext(p,page) # add page to current pages next list if not deadpage
+            #     self.buildGraph(page) # recursively build graph
 
     # find if a page with giving url existing in graph or not
-    def __findPage(self, url):
+    def findPage(self, url):
         for p in self.pages:
             if p.url == url:
                 return p
     # find if a page is in the dead_page list
-    def __findDeadPage(self, url):
+    def findDeadPage(self, url):
         for p in self.dead_pages:
             if p.url == url:
                 return p
     # add a page to graph if the page is not in graph yet
-    def __addPage(self,p):
-        if not self.__findPage(p.url):
+    def addPage(self,p):
+        if not self.findPage(p.url):
             self.pages.append(p)
             return p
     # add a page to deadpage list if the page is not in deadpage list yet
-    def __addDeadPage(self,p):
-        if not self.__findDeadPage(p.url):
+    def addDeadPage(self,p):
+        if not self.findDeadPage(p.url):
             self.dead_pages.append(p)
             return p
-    def __addNext(self,current, next):
+    def addNext(self,current, next):
         current.next.append(next)
 
 class Page():
@@ -70,8 +106,9 @@ class Page():
         try:
             self.doc = urlopen(self.url)
             self.__findInternalLinks()
-        except Exception: # TODO: need to speicify exception
+        except Exception as e: # TODO: need to speicify exception
             self.dead_page = True
+            # print str(e)
     # if url is the same, page is the same
     def __eq__(self,other):
         return self.url == other.url
@@ -104,6 +141,7 @@ def mapper():
     graph=Graph()
     graph.buildGraph(mp)
     # Testing
+    print len(graph.pages)
     for p in graph.pages:
         print p.url
 
